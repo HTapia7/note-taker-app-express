f0fae7e879223b61c6a710cc34d1cd4ecc184a71
Hernan Tapia <soliditize@MacBook-Pro.local>
2022-12-05 21:50:17 -0800

    initia code

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Note Taker Application 
+
+![Alt text](https://file%2B.vscode-resource.vscode-cdn.net/Users/soliditize/Desktop/noteTaker%202/Develop/images/Screenshot%202022-12-02%20at%2011.39.26%20PM.png?version%3D1670053437637)
+
+
+![Alt text](https://file%2B.vscode-resource.vscode-cdn.net/Users/soliditize/Desktop/noteTaker%202/Develop/images/Screenshot%202022-12-02%20at%2011.40.43%20PM.png?version%3D1670053454970)
+
+# Acceptance Criteria 
+
+* WHEN I open the Note Taker THEN I am presented with a landing page with a link to a notes page
+
+* WHEN I click on the link to the notes page THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+
+* WHEN I enter a new note title and the note’s text THEN a Save icon appears in the navigation at the top of the page
+
+* WHEN I click on the Save icon THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+
+* WHEN I click on an existing note in the list in the left-hand column THEN that note appears in the right-hand column
+
+* WHEN I click on the Write icon in the navigation at the top of the page THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+# Technologies 
+
+- HTML 
+- CSS 
+- Bootstrap 
+- NODE 
+- EXPRESS
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+const uuid = require('uuid')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('./public'));
+
+
+
+// read json file
+app.get('/api/notes' , (req , res) => {
+  const noteData = fs.readFileSync('./db/db.json')
+  res.json(JSON.parse(noteData));
+})
+
+// MODULE 11  
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you)
+
+app.post('/api/notes' , (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  notes.push(newNote);
+  fs.writeFileSync('./db/db.json' , JSON.stringify(notes));
+  res.json(notes);
+})
+
+
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+ app.delete('/api/notes/:id' , (req, res) => {
+console.log('it works')
+
+})
+
+
+// routes for index and notes
+app.get('/', (req , res) => {
+    res.sendFile(path.join(__dirname , './public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname , './public/notes.html'))
+})
+
+app.get('*' , (req , res) => {
+  res.sendFile(path.join(__dirname , './public/index.html'))
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
